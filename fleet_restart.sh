266eee96f48da2c5aa1cd1c0eaeaf27a76aa4085
Troy Fontaine <troyfontaine@users.noreply.github.com>
2016-09-22 20:52:22 -0500

    Initial commit
    Initial commit

---
+++ fleet_restart.sh
@@ -0,0 +1,129 @@
+# !/bin/bash
+# Script to use Fleetctl to stop and start containers using a rolling restart
+# By Troy Fontaine with rolling restart script from http://engineering.rainchasers.com/coreos/fleet/2015/03/03/rolling-unit-restart.html
+
+container_array=($(fleetctl list-units -fields=unit -no-legend | cut -f2 ))
+
+# Declare variables
+container=""
+clean_container=()
+
+for entry in "${container_array[@]}"
+do
+        #echo "$entry"
+        if [ ${#entry} -le 10 ]; then
+
+                # Skip to next entry
+                shift
+
+        elif [[ $entry == *"@"* ]]; then
+
+                # Lob off the last 10 characters
+                clean_container+=("${entry%??????????}")
+                echo $clean_container
+        else
+
+                clean_container+=${entry%????????}
+                echo $clean_container
+        fi
+
+
+done
+
+container_list=($(printf "%s\n" "${clean_container[@]}" | sort -u)); echo "${uniq[@]}"
+
+restart ()
+{
+		
+	fleetctl list-units | grep $1@ | cut -f1 -d. | while read -r unit ; do
+	unit_index=`echo $unit | cut -f2 -d@`
+
+	printf "unit:> %s index:> %s\n" $unit $unit_index
+
+	printf "stopping:> %s\n" $unit
+	fleetctl stop $unit
+
+	printf "waiting:> for %s to stop " $unit;
+	is_running=1
+	while [ $is_running -ne 0 ]; do
+	is_running=`fleetctl list-units | grep running | grep $unit | wc -l`;
+	sleep 1;
+	printf ".";
+	done
+	printf "\n"
+
+	printf "starting:> %s\n" $unit
+	fleetctl start $unit
+
+	printf "waiting:> for %s to start " $unit;
+	while [ $is_running -eq 0 ]; do
+	is_running=`fleetctl list-units | grep running | grep $unit | wc -l`;
+	sleep 1;
+	printf ".";
+	done
+	printf "\n"
+
+	fleetctl list-units | grep $unit
+	done
+
+}
+
+usage () 
+{
+        echo "script to restart all commonly updated services"
+        echo
+        echo "To recreate all containers:"
+        echo "$0 all"
+        echo
+        echo "To start one container"
+        echo "$0 <containername> (e.g. $0 fraud_service)"
+        echo
+        echo "To recreate one or a few containers pass them as a space seperated list:"
+        echo "$0 http_service fraud_service card_ui"
+        echo
+}
+
+if [ "$#" == 0 ]; then
+        usage
+fi
+
+if [ "$#" -ge 1 ]; then
+
+	if [ "$1" == "all" ]; then
+		
+		echo "Restarting all units individually by service"
+		for unit_name in "${container_list[@]}"
+		do
+			
+			restart "$unit_name"
+			
+		done
+		exit 0
+
+	elif [ "$1" == "testall" ]; then
+		
+		echo "Restarting all units individually by service"
+		for unit_name in "${container_list[@]}"
+		do
+			echo "Test Restart $unit_name"
+		done
+		exit 0
+
+	elif [ "$1" == "test" ]; then
+		
+		for unit_name in "$@"
+		do
+			echo "Test ad-hoc restart of $unit_name"
+		done
+	   exit 0
+
+	else
+	   
+	   for unit_name in "$@"
+		do
+			echo "Ad-hoc restart of $unit_name"
+			restart $unit_name
+		done
+	   exit 0
+	fi
+fi 
